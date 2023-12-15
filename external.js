a0fc04c007adfc7a6886983d3ce617d993b2f47e
nishanth09m <nish09work@gmail.com>
2023-12-15 17:09:45 +0530

    Add color picker button to pick the colors Add randomization of colors

--- external.js
+++ external.js
@@ -1,32 +1,41 @@
 let size = 16;
+let randomColor = false;
 function egrid(size){
- for(let i = 0; i < size; i++){
-    let div = document.createElement('div');
-    div.setAttribute('id',`${i}`);
-    div.setAttribute('style', 'background: green; height: 35px; width: 560px;display: flex;flex: 1;');    
-    main.appendChild(div);
-    for(let j = 0; j < size; j++){
-        let div1 = document.createElement('div');
-        div1.setAttribute('id',`${i}` + `${j}`);
-        div1.setAttribute('style', 'background: blue; width: 35px; display: flex; flex-grow: 1;');    
+    for(let i = 0; i < size; i++){
+        let div = document.createElement('div');
+        div.setAttribute('id',`${i}`);
+        div.setAttribute('style', 'height: 35px; width: 560px;display: flex;flex: 1;');    
+        main.appendChild(div);
+        for(let j = 0; j < size; j++){
+            let div1 = document.createElement('div');
+            div1.setAttribute('id',`${i}` + `${j}`);
+            div1.setAttribute('style', 'background: grey; width: 35px; display: flex; flex-grow: 1;');    
 
-        div1.addEventListener("mouseenter", (event) => {
-            div1.style.backgroundColor = 'black';
-        })
+            div1.addEventListener("mouseenter", (event) => {
+                if(randomColor){
+                    div1.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+                }else{
+                    let picker = document.querySelector('#colorpicker');             
+                    div1.style.backgroundColor = picker.value;
+                }
+            });
 
-        div.appendChild(div1);
+            div.appendChild(div1);
+        }
     }
 }
-}
+
+let main = document.querySelector('#container');
+let clear = document.querySelector('#clear');
+let random = document.querySelector('#randomcolor');
+egrid(size);
+
 
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
 }
-
-let main = document.querySelector('#container');
-egrid(size);
 
 let btn = document.querySelector('#btn');
 btn.addEventListener('click', (event) =>{
@@ -35,11 +44,13 @@ btn.addEventListener('click', (event) =>{
     egrid(size);
 });
 
-let clear = document.querySelector('#clear');
 clear.addEventListener('click', () => {
     removeAllChildNodes(main);
     egrid(size);
-})
+});
 
+random.addEventListener('click',() => {
+    randomColor = true;
+});
 
 

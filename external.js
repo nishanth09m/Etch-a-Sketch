951d3edefea970e3e302d5bbda6eec726c398b91
nishanth09m <nish09work@gmail.com>
2023-12-15 13:07:12 +0530

    Add change grid size functionality Add a button to manually change grid size

--- external.js
+++ external.js
@@ -1,22 +1,38 @@
-
-
- for(let i = 0; i < 16; i++){
-    let main = document.querySelector('#container');
+function egrid(size){
+ for(let i = 0; i < size; i++){
     let div = document.createElement('div');
     div.setAttribute('id',`${i}`);
-    div.setAttribute('style', 'background: white; height: 35px; width: 560px;display: flex;');    
+    div.setAttribute('style', 'background: green; height: 35px; width: 560px;display: flex;flex: 1;');    
     main.appendChild(div);
-    for(let j = 0; j < 16; j++){
+    for(let j = 0; j < size; j++){
         let div1 = document.createElement('div');
         div1.setAttribute('id',`${i}` + `${j}`);
-        div1.setAttribute('style', 'background: blue; height: 35px; width: 35px;');    
+        div1.setAttribute('style', 'background: blue; width: 35px; display: flex; flex-grow: 1;');    
 
         div1.addEventListener("mouseenter", (event) => {
             div1.style.backgroundColor = 'black';
         })
 
         div.appendChild(div1);
-
     }
 }
+}
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+let main = document.querySelector('#container');
+egrid(16);
+
+let btn = document.querySelector('#btn');
+btn.addEventListener('click', (event) =>{
+    let size = Number(prompt("Enter your prefered grid size?"));
+    removeAllChildNodes(main);
+    egrid(size);
+})
+
+
 
